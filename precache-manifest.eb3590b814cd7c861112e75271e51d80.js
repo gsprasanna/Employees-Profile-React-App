c638e99ad65136cee0f718d4b4fec0be76970d05
Prasanna Balaji <erprasanna97@gmail.com>
2020-01-08 01:10:23 +0530

    Updates

---
+++ precache-manifest.eb3590b814cd7c861112e75271e51d80.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b26983a609753a741a900f338598bf2",
+    "url": "/Employees-Profile-React-App/index.html"
+  },
+  {
+    "revision": "597224157f54236eb3d5",
+    "url": "/Employees-Profile-React-App/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "030d72d512cfc24c2d1c",
+    "url": "/Employees-Profile-React-App/static/css/main.3ba5cd3e.chunk.css"
+  },
+  {
+    "revision": "597224157f54236eb3d5",
+    "url": "/Employees-Profile-React-App/static/js/2.77915900.chunk.js"
+  },
+  {
+    "revision": "409e88028c833aab3b5c4200318b3af2",
+    "url": "/Employees-Profile-React-App/static/js/2.77915900.chunk.js.LICENSE"
+  },
+  {
+    "revision": "030d72d512cfc24c2d1c",
+    "url": "/Employees-Profile-React-App/static/js/main.e79d8278.chunk.js"
+  },
+  {
+    "revision": "f27626eac883528f4433",
+    "url": "/Employees-Profile-React-App/static/js/runtime-main.82457526.js"
+  },
+  {
+    "revision": "81c29bc5316124abc2c07a92ebbc23e6",
+    "url": "/Employees-Profile-React-App/static/media/loading.81c29bc5.gif"
+  }
+]);
